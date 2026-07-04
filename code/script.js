6c9f125b34a2a8ac97a4e8df25eded9cc4b43061
Sljivoa14 <amarsljivo7@gmail.com>
2026-07-04 18:14:51 +0200

    pt 2

--- code/script.js
+++ code/script.js
@@ -1,3 +1,46 @@
 const canvasBoard = document.getElementById('canvas');
 const ctx = canvasBoard.getContext('2d');
 const resetBtn = document.getElementById('reset')
+
+const width = canvasBoard.width;
+const height = canvasBoard.height;
+
+const gravity = 0.5;
+const flapStrenhgth = -10;
+const moveSpeed = 2;
+const pipeWidth = 40;
+const pipeHeight = Math.floor(Math.random() * 200) + 50;
+const pipeGame = 150;
+const pipeInterval = 150;
+const pipeSpeed = 2;
+
+let bird = {
+    x: 120,
+    y: height / 2,
+    radius: 16,
+    velocity: 0,
+
+};
+
+let pipes = [];
+let score = 0;
+let gameOver = false;
+let bestScore = 0;
+
+function drawBird(x, y){
+
+}
+
+function resetGame() {
+  bird = {
+    x: 120,
+    y: height / 2,
+    radius: 16,
+    velocity: 0,
+  };
+  pipes = [];
+  frame = 0;
+  score = 0;
+  gameOver = false;
+  loop();
+}
